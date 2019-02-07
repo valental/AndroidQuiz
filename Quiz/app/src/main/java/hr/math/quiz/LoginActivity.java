3b4339b8546dce50a5e2dde1a2cfb284cc7e66f1
Luka Valenta <valenta.luka@gmail.com>
2019-02-07 14:58:54 +0100

    Login activity added.

--- Quiz/app/src/main/java/hr/math/quiz/LoginActivity.java
+++ Quiz/app/src/main/java/hr/math/quiz/LoginActivity.java
@@ -5,20 +5,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class RegisterActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        setContentView(R.layout.activity_login);
     }
 
-    public void OpenLogin(View view) {
-        Intent intent = new Intent(this, LoginActivity.class);
+    public void Login(View view) {
+    }
+
+    public void OpenRegister(View view) {
+        Intent intent = new Intent(this, RegisterActivity.class);
         startActivity(intent);
-    }
-
-    public void Register(View view) {
-
     }
 }
