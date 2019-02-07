95d408c10e18a7e8b16390ea19e7b0d8370b1253
Luka Valenta <valenta.luka@gmail.com>
2019-02-07 17:21:42 +0100

    Added buttons and option menu to main activity and user login is saved.

--- Quiz/app/src/main/java/hr/math/quiz/MainActivity.java
+++ Quiz/app/src/main/java/hr/math/quiz/MainActivity.java
@@ -3,7 +3,12 @@ package hr.math.quiz;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,8 +18,26 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void OpenRegister(View view) {
-        Intent intent = new Intent(this, RegisterActivity.class);
-        startActivity(intent);
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_activity_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        if (R.id.logout == item.getItemId()) {
+            Toast.makeText(this, "You clicked on Item 1", Toast.LENGTH_LONG).show();
+            return true;
+        }
+        return false;
+    }
+
+    public void ClearPreferences(View view) {
+        PreferencesManager preferencesManager = new PreferencesManager(this);
+        preferencesManager.SaveUsername("");
     }
 }
