11c4b283ac0f3d52260620844489c9cfcd5653f1
Luka Valenta <valenta.luka@gmail.com>
2019-02-09 20:05:50 +0100

    Added controls to all scores activity.

--- Quiz/app/src/main/java/hr/math/quiz/AllScoresActivity.java
+++ Quiz/app/src/main/java/hr/math/quiz/AllScoresActivity.java
@@ -6,6 +6,11 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AllScoresActivity extends AppCompatActivity {
 
@@ -13,6 +18,44 @@ public class AllScoresActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_scores);
+
+        LoadScores();
+    }
+
+    private void LoadScores() {
+        // TODO get the best player in each category
+        String[] userMovies = {"Igrač1", "Igrač2", "Igrač3"};
+        String[] userSport = {"Igrač4", "Igrač5", "Igrač6"};
+        String[] userScience = {"Igrač7", "Igrač8", "Igrač9"};
+        String[] userGeography = {"Igrač1", "Igrač2", "Igrač3"};
+        String[] userHistoryAndArt = {"Igrač1", "Igrač2", "Igrač3"};
+
+        AddScoreBoard(R.id.moviesPlayersLL, userMovies);
+        AddScoreBoard(R.id.sportPlayersLL, userSport);
+        AddScoreBoard(R.id.sciencePlayersLL, userScience);
+        AddScoreBoard(R.id.geographyPlayersLL, userGeography);
+        AddScoreBoard(R.id.historyAndArtPlayersLL, userHistoryAndArt);
+    }
+
+    private void AddScoreBoard(int parentLayoutId, String[] players){
+        LinearLayout innerContainer = findViewById(parentLayoutId);
+        for (int i = 0; i < players.length; i++) {
+            LinearLayout linearLayout = new LinearLayout(this);
+            linearLayout.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT));
+            linearLayout.setOrientation(LinearLayout.HORIZONTAL);
+
+            TextView position = new TextView(this);
+            position.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.MATCH_PARENT));
+            position.setText(Integer.toString(i+1) + ". ");
+            linearLayout.addView(position);
+
+            TextView player = new TextView(this);
+            player.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT));
+            player.setText(players[i]);
+            linearLayout.addView(player);
+
+            innerContainer.addView(linearLayout);
+        }
     }
 
     @Override
