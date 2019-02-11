5c39a3e8e29292aa47aa0d74d0b785459f203c9d
Dajana Jeroncic <djeroncic@gmail.com>
2019-02-11 12:14:39 +0100

    Add server logout service

--- Quiz/app/src/main/java/hr/math/quiz/api/ApiRequest.java
+++ Quiz/app/src/main/java/hr/math/quiz/api/ApiRequest.java
@@ -7,6 +7,8 @@ import hr.math.quiz.api.models.Session;
 import hr.math.quiz.api.models.User;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -42,5 +44,21 @@ public class ApiRequest {
         Call<ResponseBody> levelCall = service.upgradeLevel(result, Authorization);
 
         return levelCall;
+    }
+
+    public static void logoutUser(String token) {
+        Call<ResponseBody> logoutCall = service.logoutUser(token);
+
+        logoutCall.enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                // do nothing
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+                // do nothing
+            }
+        });
     }
 }
